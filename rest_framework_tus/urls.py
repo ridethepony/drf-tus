dafdd62968765f770a6d1c17ebf12368cca0f326
Wouter de Vries <wouter@oxyzen.nl>
2022-01-05 11:00:48 +0100

    Fix compatibility with django-rest-framework>=3.11

--- rest_framework_tus/urls.py
+++ rest_framework_tus/urls.py
@@ -5,8 +5,8 @@ from rest_framework_tus.views import UploadViewSet
 from .routers import TusAPIRouter
 
 router = TusAPIRouter()
-router.register(r'files', UploadViewSet, base_name='upload')
+router.register(r'files', UploadViewSet, basename='upload')
 
 urlpatterns = [
-    url(r'', include(router.urls, namespace='api'))
+    url(r'', include((router.urls, 'rest_framework_tus'), namespace='api'))
 ]
--- tests/urls.py
+++ tests/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import include, url
 from rest_framework_tus.urls import urlpatterns as rest_framework_tus_urls
 
 urlpatterns = [
-    url(r'^', include(rest_framework_tus_urls, namespace='rest_framework_tus')),
+    url(r'^', include((rest_framework_tus_urls, 'rest_framework_tus_tests'), namespace='rest_framework_tus')),
 ]
