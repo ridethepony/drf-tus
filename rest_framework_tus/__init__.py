adc8a3e061b086afdd06a5f258bfd85b81113a23
Thomas Kalverda <tkalverda@leukeleu.nl>
2022-03-02 15:42:57 +0100

    fix deprecation warnings

--- rest_framework_tus/__init__.py
+++ rest_framework_tus/__init__.py
@@ -1,6 +1,5 @@
-default_app_config = 'rest_framework_tus.apps.RestFrameworkTusConfig'
 
-__version__ = '1.21.3dev'
+__version__ = '1.21.4dev'
 
 tus_api_version = '1.0.0'
 tus_api_version_supported = ['1.0.0']
