9c48eac83d1e7cb7a85f067d17eb03c092f60e23
Ramon de Jezus <rdejezus@leukeleu.nl>
2022-03-16 09:45:53 +0100

    Fix zero-byte uploads

--- rest_framework_tus/views.py
+++ rest_framework_tus/views.py
@@ -93,18 +93,19 @@ class TusCreateMixin(mixins.CreateModelMixin):
             return Response('Missing "{}" header.'.format('Tus-Resumable'), status=status.HTTP_400_BAD_REQUEST)
 
         # Get file size from request
-        upload_length = getattr(request, constants.UPLOAD_LENGTH_FIELD_NAME, -1)
+        upload_length = getattr(request, constants.UPLOAD_LENGTH_FIELD_NAME, None)
 
         # Validate upload_length
         max_file_size = getattr(self, 'max_file_size', tus_settings.TUS_MAX_FILE_SIZE)
-        if upload_length > max_file_size:
-            return Response('Invalid "Upload-Length". Maximum value: {}.'.format(max_file_size),
-                            status=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
 
-        # If upload_length is not given, we expect the defer header!
-        if not upload_length or upload_length < 0:
+        if upload_length is None:  # We want to allow 0
+            # If upload_length is not given, we expect the defer header!
             if getattr(request, constants.UPLOAD_DEFER_LENGTH_FIELD_NAME, -1) != 1:
                 return Response('Missing "{Upload-Defer-Length}" header.', status=status.HTTP_400_BAD_REQUEST)
+        else:
+            if upload_length > max_file_size:
+                return Response('Invalid "Upload-Length". Maximum value: {}.'.format(tus_settings.TUS_MAX_FILE_SIZE),
+                                status=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
 
         # Get metadata from request
         upload_metadata = getattr(request, constants.UPLOAD_METADATA_FIELD_NAME, {})
